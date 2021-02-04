5971001663eca115467e2dbaa25f1b39f49d672b
anjalisoni3655 <anjalisony32@gmail.com>
2021-02-04 13:07:28 +0530

    cards and footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 import "./App.css";
 import Footer from "./components/footer";
 import About from "./components/about";
-import Home from "./components/home";
+import ImgMediaCard from "./components/cardUser";
+import RecipeReviewCard from "./components/cardProjects";
+import { Card, Grid, Icon } from "tabler-react";
+
+//import { Row, Col, Grid } from "react-bootstrap";
 
 //import { render } from "react-dom";
 import Navbar from "./components/Navbar";
@@ -10,10 +14,22 @@ import Navbar from "./components/Navbar";
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar></Navbar>
-        <About></About>
-        <Footer></Footer>
+      <div className="maincontainer">
+        <Grid.Row>
+          <Grid.Col>
+            <Navbar></Navbar>
+          </Grid.Col>
+
+          <Grid.Col>
+            <ImgMediaCard></ImgMediaCard>
+          </Grid.Col>
+          <Grid.Col>
+            <RecipeReviewCard></RecipeReviewCard>
+          </Grid.Col>
+          <Grid.Col>
+            <Footer></Footer>
+          </Grid.Col>
+        </Grid.Row>
       </div>
     );
   }
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import "./footer.css";
 
-
 class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-       
         <p>Some footer !</p>
       </footer>
     );
