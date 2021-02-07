6fae548abf8d8d7dfbea3ad74ee70cd8ac8a0996
Aitik Gupta <aitikgupta@gmail.com>
2021-02-08 04:20:21 +0530

    Update pod information

--- src/content/TeamDetails.js
+++ src/content/TeamDetails.js
@@ -1,62 +1,113 @@
-import anjali from "../resources/TeamMembers/anjii.jpg";
+import Aitik from "../resources/TeamMembers/Aitik.png";
+import Anjali from "../resources/TeamMembers/Anjali.jpg";
+import Bodhisha from "../resources/TeamMembers/Bodhisha.jpeg";
+import Bushra from "../resources/TeamMembers/Bushra.jpeg";
+import Dean from "../resources/TeamMembers/Dean.jpg";
+import Deepak from "../resources/TeamMembers/Deepak.jpg";
+import Marco from "../resources/TeamMembers/Marco.jpg";
+import Rashi from "../resources/TeamMembers/Rashi.jpg";
+import Rashika from "../resources/TeamMembers/Rashika.jpg";
+import Samuel from "../resources/TeamMembers/Samuel.jpeg";
+import Sena from "../resources/TeamMembers/Sena.jpeg";
+import Shrill from "../resources/TeamMembers/Shrill.jpg";
 
 const teamDetails = [
   [
     {
-      name: "Anjali Soni",
-      position: "Full Stack Developer",
-      img: anjali,
-      email: "anjalisoni@gmail.com",
+      name: "Rashika Karki",
+      position: "Nepal",
+      img: Rashika,
+      email: "rashikakarki9841@gmail.com",
+    },
+    {
+      name: "Aitik Gupta",
+      position: "India",
+      img: Aitik,
+      email: "aitikgupta@gmail.com",
     },
     {
       name: "Anjali Soni",
-      position: "Full Stack Developer",
-      img: anjali,
-      email: "anjalisoni@gmail.com",
-    },
-  ],
-  [
-    {
-      name: "Anjali Soni",
-      position: "Full Stack Developer",
-      img: anjali,
-      email: "anjalisoni@gmail.com",
-    },
-  ],
-  [
-    {
-      name: "Anjali Soni",
-      position: "Full Stack Developer",
-      img: anjali,
-      email: "anjalisoni@gmail.com",
+      position: "India",
+      img: Anjali,
+      email: "anjalisoni3655@gmail.com",
     },
     {
-      name: "Anjali Soni",
-      position: "Full Stack Developer",
-      img: anjali,
-      email: "anjalisoni@gmail.com",
-    },
-  ],
-  [
-    {
-      name: "Anjali Soni",
-      position: "Full Stack Developer",
-      img: anjali,
-      email: "anjalisoni@gmail.com",
-    },
-  ],
-  [
-    {
-      name: "Anjali Soni",
-      position: "Full Stack Developer",
-      img: anjali,
-      email: "anjalisoni@gmail.com",
+      name: "Bodhisha Thomas",
+      position: "India",
+      img: Bodhisha,
+      email: "bodhisha@gmail.com",
     },
     {
-      name: "Anjali Soni",
-      position: "Full Stack Developer",
-      img: anjali,
-      email: "anjalisoni@gmail.com",
+      name: "Bushra Akram",
+      position: "Pakistan",
+      img: Bushra,
+      email: "bushra.akram999@hotmail.com",
+    },
+    {
+      name: "Dean Gladish",
+      position: "United States of America",
+      img: Dean,
+      email: "gladish.dean@gmail.com",
+    },
+    {
+      name: "Deepak Agrawal",
+      position: "India",
+      img: Deepak,
+      email: "debug.career@gmail.com",
+    },
+    {
+      name: "Marco Chan",
+      position: "Canada",
+      img: Marco,
+      email: "marco.chan30@gmail.com",
+    },
+    {
+      name: "Prashi Doval",
+      position: "India",
+      img: Anjali,
+      email: "anjalisoni3655@gmail.com",
+    },
+    {
+      name: "Rashi Sharma",
+      position: "India",
+      img: Rashi,
+      email: "rashii8791@gmail.com",
+    },
+    {
+      name: "Samuel Yuan",
+      position: "United States of America",
+      img: Samuel,
+      email: "samuel6161@gmail.com",
+    },
+    {
+      name: "Sena Yevenyo",
+      position: "United States of America",
+      img: Sena,
+      email: "yevenyos@lafayette.edu",
+    },
+    {
+      name: "Shrill Shrestha",
+      position: "United Stated of America",
+      img: Shrill,
+      email: "shrill.shrestha27@gmail.com",
+    },
+    {
+      name: "Steve Tey",
+      position: "United States of America",
+      img: Anjali,
+      email: "anjalisoni3655@gmail.com",
+    },
+    {
+      name: "Sumi Kolli",
+      position: "United States of America",
+      img: Anjali,
+      email: "anjalisoni3655@gmail.com",
+    },
+    {
+      name: "Yvon Manzi",
+      position: "United States of America",
+      img: Anjali,
+      email: "anjalisoni3655@gmail.com",
     },
   ],
 ];
