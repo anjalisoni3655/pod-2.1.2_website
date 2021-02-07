03a371b2a7e553fa0db00b41f88f2e5daf171ac5
anjalisoni3655 <anjalisony32@gmail.com>
2021-02-08 05:29:28 +0530

    fixed photos

--- src/content/TeamDetails.js
+++ src/content/TeamDetails.js
@@ -1,5 +1,5 @@
 import Aitik from "../resources/TeamMembers/Aitik.png";
-import Anjali from "../resources/TeamMembers/Anjali.jpg";
+import Anjali from "../resources/TeamMembers/anjali.jpg";
 import Bodhisha from "../resources/TeamMembers/Bodhisha.jpeg";
 import Bushra from "../resources/TeamMembers/Bushra.jpeg";
 import Dean from "../resources/TeamMembers/Dean.jpg";
@@ -12,31 +12,37 @@ import Sena from "../resources/TeamMembers/Sena.jpeg";
 import Shrill from "../resources/TeamMembers/Shrill.jpg";
 
 const teamDetails = [
+  
+    [
+      {
+        name: "Rashika Karki",
+        position: "Nepal",
+        img: Rashika,
+        email: "rashikakarki9841@gmail.com",
+      },
+    ],
+    [
+      {
+        name: "Aitik Gupta",
+        position: "India",
+        img: Aitik,
+        email: "aitikgupta@gmail.com",
+      },
+      {
+        name: "Anjali Soni",
+        position: "India",
+        img: Anjali,
+        email: "anjalisoni3655@gmail.com",
+      },
+      {
+        name: "Bodhisha Thomas",
+        position: "India",
+        img: Bodhisha,
+        email: "bodhisha@gmail.com",
+      },
+    ],
+  
   [
-    {
-      name: "Rashika Karki",
-      position: "Nepal",
-      img: Rashika,
-      email: "rashikakarki9841@gmail.com",
-    },
-    {
-      name: "Aitik Gupta",
-      position: "India",
-      img: Aitik,
-      email: "aitikgupta@gmail.com",
-    },
-    {
-      name: "Anjali Soni",
-      position: "India",
-      img: Anjali,
-      email: "anjalisoni3655@gmail.com",
-    },
-    {
-      name: "Bodhisha Thomas",
-      position: "India",
-      img: Bodhisha,
-      email: "bodhisha@gmail.com",
-    },
     {
       name: "Bushra Akram",
       position: "Pakistan",
@@ -55,24 +61,28 @@ const teamDetails = [
       img: Deepak,
       email: "debug.career@gmail.com",
     },
+  ],
+  [
     {
-      name: "Marco Chan",
-      position: "Canada",
-      img: Marco,
-      email: "marco.chan30@gmail.com",
-    },
-    {
-      name: "Prashi Doval",
-      position: "India",
+      name: "Steve Tey",
+      position: "United States of America",
       img: Anjali,
       email: "anjalisoni3655@gmail.com",
     },
     {
-      name: "Rashi Sharma",
-      position: "India",
-      img: Rashi,
-      email: "rashii8791@gmail.com",
+      name: "Sumi Kolli",
+      position: "United States of America",
+      img: Anjali,
+      email: "anjalisoni3655@gmail.com",
     },
+    {
+      name: "Yvon Manzi",
+      position: "United States of America",
+      img: Anjali,
+      email: "anjalisoni3655@gmail.com",
+    },
+  ],
+  [
     {
       name: "Samuel Yuan",
       position: "United States of America",
@@ -91,23 +101,25 @@ const teamDetails = [
       img: Shrill,
       email: "shrill.shrestha27@gmail.com",
     },
+  ],
+  [
     {
-      name: "Steve Tey",
-      position: "United States of America",
+      name: "Marco Chan",
+      position: "Canada",
+      img: Marco,
+      email: "marco.chan30@gmail.com",
+    },
+    {
+      name: "Prashi Doval",
+      position: "India",
       img: Anjali,
       email: "anjalisoni3655@gmail.com",
     },
     {
-      name: "Sumi Kolli",
-      position: "United States of America",
-      img: Anjali,
-      email: "anjalisoni3655@gmail.com",
-    },
-    {
-      name: "Yvon Manzi",
-      position: "United States of America",
-      img: Anjali,
-      email: "anjalisoni3655@gmail.com",
+      name: "Rashi Sharma",
+      position: "India",
+      img: Rashi,
+      email: "rashii8791@gmail.com",
     },
   ],
 ];
